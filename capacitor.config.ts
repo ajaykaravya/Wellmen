8fed4981ae0d7209a7a0b43cfb2f40f92d39ff10
ajaykaravya <ajayb.karavyasolutions@gmail.com>
2026-04-25 16:57:11 +0530

    update capacitor config file

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,7 +5,7 @@ const config: CapacitorConfig = {
   appName: 'wellman',
   webDir: 'out',
   server: {
-    url: 'https://wellmen-ozt99e0f8-ajaybkaravyasolutions-3883s-projects.vercel.app', // 👈 YOUR LIVE URL
+    url: 'https://wellmen-git-main-ajaybkaravyasolutions-3883s-projects.vercel.app', // 👈 YOUR LIVE URL
     cleartext: false
   }
 };
